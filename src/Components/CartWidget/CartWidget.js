7f47f711d5032e328f3cb65a189cb477e7ecf6c7
MrPabloVS <pablovazquezsoldano@gmail.com>
2021-09-24 18:00:12 -0300

    Primer Commit

---
+++ src/Components/CartWidget/CartWidget.js
@@ -0,0 +1,11 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
+function CartWidget() {
+    return(
+        <FontAwesomeIcon icon="fa-solid fa-cart-shopping" />
+    );
+}
+
+
+export default CartWidget
---
+++ src/Components/ItemListContainer/ItemListContainer.js
@@ -0,0 +1,14 @@
+
+
+function ItemListContainer() {
+    
+    
+    return (
+      <div className="bg-black">
+          <p className="text-light text-center">No hay articulos disponibles.</p>
+      </div>
+    );
+  }
+  
+  export default ItemListContainer;
+  
