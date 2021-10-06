86fdcba6100b10be777ac768eae17c43ff7eb3ef
MrPabloVS <pablovazquezsoldano@gmail.com>
2021-10-06 17:09:49 -0300

    ItemList Pseudo Funcional

--- src/Components/ItemList/ItemList.js
+++ src/Components/ItemList/ItemList.js
@@ -3,13 +3,15 @@ import React, {useState, useEffect} from 'react';
 
 
 function ItemList() {
-    const [result, setResult] = useState([])
     const lista = [
-            {id:1,itemName:"Iphone 6",Price:"300000$",stock:10 ,img:"https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP705/SP705-iphone_6-mul.png"},
-            {id:2,itemName:"Samsung S21",Price:"50000$",stock:3, img:"https://i.blogs.es/d9faf7/samsung-galaxy-s21-ultra-00-02/450_1000.jpg"},
-            {id:3,itemName:"Alcatel 1V",Price:"70000$",stock:6, img:"https://i.blogs.es/6947e3/alcatel1v2020/450_1000.jpg"},
-            {id:4,itemName:"Iphone XS",Price:"1000000$",stock:4, img:"https://m.media-amazon.com/images/I/81wcv7XP3cL._AC_SL1500_.jpg"}
+        {id:1,itemName:"Iphone 6",Price:"300000$",stock:10 ,img:"https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP705/SP705-iphone_6-mul.png"},
+        {id:2,itemName:"Samsung S21",Price:"50000$",stock:3, img:"https://i.blogs.es/d9faf7/samsung-galaxy-s21-ultra-00-02/450_1000.jpg"},
+        {id:3,itemName:"Alcatel 1V",Price:"70000$",stock:6, img:"https://i.blogs.es/6947e3/alcatel1v2020/450_1000.jpg"},
+        {id:4,itemName:"Iphone XS",Price:"1000000$",stock:4, img:"https://m.media-amazon.com/images/I/81wcv7XP3cL._AC_SL1500_.jpg"}
     ]
+    const [result, setResult] = useState([])
+    
+    
 
     const task = new Promise((resolve, reject)=>{
         setTimeout(() => {
@@ -19,7 +21,7 @@ function ItemList() {
     useEffect(() => {
         task.then(res =>{
             console.log(res)
-            setResult(res)
+            setResult(lista)
         }, err=>{
             console.log(err)
         }).finally(()=>{
@@ -27,12 +29,14 @@ function ItemList() {
         })
         console.log(result)
         
-    }, [result])
+    }, )
     
     
     return(
         <>
-            {result.map(u=> <Item key={u.id} itemName={u.itemName} itemPrice={u.Price} itemImg={u.img} stockProp={u.stock} />) }
+        
+            {result.map(u=> <Item key={u.id}  itemName={u.itemName} itemPrice={u.Price} itemImg={u.img} stockProp={u.stock} />) }
+        
         </>
     );
 }
