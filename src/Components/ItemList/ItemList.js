bf4f7224561278de39eb2a74fdbac4b7892f782e
MrPabloVS <pablovazquezsoldano@gmail.com>
2021-10-06 16:35:40 -0300

    ItemList .map

--- src/Components/ItemList/ItemList.js
+++ src/Components/ItemList/ItemList.js
@@ -3,24 +3,31 @@ import React, {useState, useEffect} from 'react';
 
 
 function ItemList() {
-    const [result, setResult] = useState()
-
-const task = new Promise((resolve, reject)=>{
-    setTimeout(() => {
-        resolve([
+    const [result, setResult] = useState([])
+    const lista = [
             {id:1,itemName:"Iphone 6",Price:"300000$",stock:10 ,img:"https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP705/SP705-iphone_6-mul.png"},
             {id:2,itemName:"Samsung S21",Price:"50000$",stock:3, img:"https://i.blogs.es/d9faf7/samsung-galaxy-s21-ultra-00-02/450_1000.jpg"},
             {id:3,itemName:"Alcatel 1V",Price:"70000$",stock:6, img:"https://i.blogs.es/6947e3/alcatel1v2020/450_1000.jpg"},
             {id:4,itemName:"Iphone XS",Price:"1000000$",stock:4, img:"https://m.media-amazon.com/images/I/81wcv7XP3cL._AC_SL1500_.jpg"}
-        ])
-    }, 2000);
-})
-useEffect(() => {
-    task.then(res =>{
-        setResult(res)
+    ]
+
+    const task = new Promise((resolve, reject)=>{
+        setTimeout(() => {
+            resolve([lista])
+        }, 2000);
     })
-    
-}, [result])
+    useEffect(() => {
+        task.then(res =>{
+            console.log(res)
+            setResult(res)
+        }, err=>{
+            console.log(err)
+        }).finally(()=>{
+            console.log("Finalizado")
+        })
+        console.log(result)
+        
+    }, [result])
     
     
     return(
