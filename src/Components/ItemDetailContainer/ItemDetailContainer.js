b50f578e5fdc69d756949a32b44666e2460be051
MrPabloVS <pablovazquezsoldano@gmail.com>
2021-11-15 14:13:49 -0300

    ItemDetail con Firebase

--- src/Components/ItemDetailContainer/ItemDetailContainer.js
+++ src/Components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,37 +1,33 @@
 
-import axios from 'axios';
 import ItemDetail from '../ItemDetail/ItemDetail'
 import React, {useState, useEffect,} from 'react';
 import {useParams} from 'react-router'
+import Loading from '../Loading/Loading';
+import {getFirestore} from '../../sevices/getFirebase'
 
 function ItemDetailContainer() {
 
     const {id} = useParams()
     const [Produc, setProduc] = useState([])
+    const [loading, setLoading] = useState(true)
     
 
-    const getProducId = async () => {
-
-        try {
-            const respuesta = await axios.get( `https://fakestoreapi.com/products/${id}`)
-            console.log(respuesta.data)
-            setProduc(respuesta.data)
-            console.log(Produc)
-            console.log(id)
-        } catch (error) {
-            console.log(error)
-            
-        }
-    }
 
     useEffect(() => {
-        getProducId()
+
+        const dataBase = getFirestore()
+        dataBase.collection('Items').doc(id).get()
+        .then(resp =>  setProduc( { id: resp.id , ...resp.data() } ))
+        .catch(err=>console.log(err))
+        .finally(()=> setLoading(false))
+
     }, [])
 
     return(
-     <>
-         <ItemDetail item={Produc} itemName={Produc.title} itemPrice={"$" + Produc.price} itemImg={Produc.image} stockProp={5} itemId={id}></ItemDetail>       
-     </>
+     <div >
+     { loading ? <Loading/> :
+         <ItemDetail item={Produc} itemName={Produc.title} itemPrice={"$" + Produc.price} itemImg={Produc.img} stockProp={5} itemId={id}></ItemDetail>}       
+     </div>
     )
 
 }
--- src/Components/ItemList/ItemList.js
+++ src/Components/ItemList/ItemList.js
@@ -1,47 +1,14 @@
 import Item from "../Item/Item";
-import React, {useState, useEffect} from 'react';
-import axios from "axios";
 import {Row} from 'react-bootstrap'
-import Loading from "../Loading/Loading";
 
 
 
 function ItemList({lista}) {
-    //const [lista, setLista] = useState([])
-    //const [Loading, setLoading] = useState(true)
-
-    // const getlista = async () => {
-
-    //     try {
-    //         const respuesta = await axios.get( `https://fakestoreapi.com/products`)
-    //         console.log(respuesta.data)
-    //         setLista(respuesta.data)
-    //         console.log(lista)
-    //     } catch (error) {
-    //         console.log(error)
-            
-    //     }
-    // }
-
-    // const cambiarEstadoCarga = ()=> {
-    //     setTimeout(() => {
-    //         setLoading(false)
-    //     }, 5000);
-    // }
-        
     
-    
-
-
-    // useEffect(() => {
-    //     getlista()
-    //     cambiarEstadoCarga()
-    // }, []) 
     
     
     return(
         <div>   
-            {/* {Loading = false ? <Loading/> :} */}
             <Row xs={1} md={4} className="g-4">
                 {lista && lista.map(u=> <Item key={u.id}  itemName={u.title} itemPrice={"$" + u.price} itemImg={u.img} stockProp={5} itemId={u.id} />) }
             </Row>
