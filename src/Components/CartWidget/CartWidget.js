7c5892b21535414086acb021cc8e0c3858517110
MrPabloVS <pablovazquezsoldano@gmail.com>
2021-11-17 16:42:13 -0300

    Link en CartWidget

--- src/Components/CartWidget/CartWidget.js
+++ src/Components/CartWidget/CartWidget.js
@@ -2,15 +2,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartPlus } from '@fortawesome/free-solid-svg-icons'
 import { useCartContext } from '../../Context/CartContext';
 import {  Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function CartWidget() {
     const {cartList} = useCartContext()
     return(
-        <>
-            <Navbar.Brand href="#home"><p >{cartList.length}</p ></Navbar.Brand>
-            <Navbar.Brand href="#home"><FontAwesomeIcon icon={faCartPlus} /></Navbar.Brand>
+        <Link to="/cart" >
+            <Navbar.Brand href="/cart"><p >{cartList.length}</p ></Navbar.Brand>
+            <Navbar.Brand href="/cart"><FontAwesomeIcon icon={faCartPlus} /></Navbar.Brand>
         
-        </>
+        </Link>
     );
 }
 
